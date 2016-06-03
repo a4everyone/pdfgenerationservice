65c2f381e018179656a1fe9506ad2d6b4581b157
patuf <nikolay.nikolov@a4everyone.com>
2016-06-03 13:11:02 +0000

    Initial commit - all needed files .bat for windows

---
+++ onendprint.bat
@@ -0,0 +1,2 @@
+echo %1 %2 %3 %4 %5 > %1.txt
+taskkill /IM firefox.exe
---
+++ scanner.bat
@@ -0,0 +1,44 @@
+@echo off
+
+SET timeoutsecs=30
+SET bucketindir="z:\in"
+SET bucketoutdir=z:\out
+
+:cycle
+FOR %%F IN (%bucketindir%\*) DO (
+ set filename=%%~nF
+ goto printing
+)
+echo Nothing found. Rinse and repeat
+:timeoutandrepeat
+timeout %timeoutsecs%
+goto cycle
+
+:printing
+call setupprint.bat %bucketoutdir%\%filename%-en.pdf
+"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "https://www.a4everyone.com/report/print/?data=%filename%&pr"
+
+if exist success.txt (
+  ren success.txt success-en.txt
+) else (
+REM Assume failure
+  echo Bad luck. Trying again.
+  goto timeoutandrepeat
+)
+
+call setupprint.bat %bucketoutdir%\%filename%-bg.pdf
+"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "https://www.a4everyone.com/report/print/bg/?data=%filename%&pr"
+
+if exist success.txt (
+  echo deleting %bucketindir%\"%filename%"
+  del %bucketindir%\"%filename%"
+  del success*.txt
+) else (
+REM Assume failure
+  del success*.txt
+  echo Bad luck. Trying again.
+  goto timeoutandrepeat
+)
+
+echo rinse and repeat
+goto cycle
